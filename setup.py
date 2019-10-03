b08896efa6127ef597521c6067e09f6921032acf
Erdem Aybek <eaybek@gmail.com>
2019-10-03 13:57:33 +0300

    add an executable script (passgenerate)
    require exposer

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="passgenerate",
-    version="0.0.2",
+    version="0.0.3",
     author="Erdem Aybek",
     author_email="eaybek@gmail.com",
     description=" ".join(["Nothing"]),
@@ -13,11 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/eaybek/passgenerate",
     packages=setuptools.find_packages(),
+    scripts=["bin/passgenerate"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Development Status :: 1 - Planning",
     ],
+    install_requires=["exposer", "passgenerate"],
     python_requires=">=3.6",
 )
